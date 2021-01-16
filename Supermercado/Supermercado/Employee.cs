13db449de12c5ef802f21c1ec5a6fd95bae1302a
Daniela <daniela_rgoncalves@hotmail.com>
2021-01-16 23:51:38 +0000

    EditEmployee()

--- Supermercado/Supermercado/Employee.cs
+++ Supermercado/Supermercado/Employee.cs
@@ -141,6 +141,30 @@ namespace Supermercado
 
         }
 
+        public Employee EditEmployee(string id, string newName, string newPassword, EmployeeRole newEmployeeRole)
+        {
+            Employee e = FindEmployee(id);
+
+            if (e != null)
+            {
+                if (!newName.Equals(""))  // if (String.IsNullOrEmpty(novoNome)
+                {
+                    e.Name = newName;
+                }
+                if (!newPassword.Equals(""))  // if (String.IsNullOrEmpty(novoNome)
+                {
+                    e.Password = newPassword;
+                }
+                if (!newEmployeeRole.Equals(""))  // if (String.IsNullOrEmpty(novoNome)
+                {
+                    e.EmployeeRole = newEmployeeRole;
+                }
+                GravarParaFicheiro();
+                return e;
+            }
+            return null;
+        }
+
         public bool RemoveEmployee(string id)
         {
             int indexAremover = -1;
