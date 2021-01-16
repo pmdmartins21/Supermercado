4044cd474c0c08a1bdc86c5f800b0b3b5f04d3c5
Daniela <daniela_rgoncalves@hotmail.com>
2021-01-16 18:27:49 +0000

    Fruits and Vegetables

--- Supermercado/Supermercado/FruitsVegetables.cs
+++ Supermercado/Supermercado/FruitsVegetables.cs
@@ -4,15 +4,16 @@ using System.Text;
 
 namespace Supermercado
 {
+    enum Section
+    {
+        Legumes,
+        Frutas,
+        verduras
+    }
     class FruitsVegetables : Product
     {
-        enum Section
-        {
-           Legumes,
-           Frutas,
-           verduras
-        }
-        public Section section { get => section; set => section = value; }
+        private Section section;
+        internal Section Section { get => section; set => section = value; }
 
         public FruitsVegetables(string id, string name, float stock, float unitPrice, TypeOfProducts typeOfProducts, Category category, Section section) : base(id, name, stock, unitPrice, typeOfProducts, category)
         {
